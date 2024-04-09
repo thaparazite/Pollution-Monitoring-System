3f2a237a67eaf720da2a86dcf883e45eb7857908
Flaviu Vanca <skety_tm@yahoo.com.com>
2024-04-09 14:04:57 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -1,1 +1,8 @@
-console.log('Server-side code running');
+const grpc = require('grpc');// import grpc module
+const protoLoader = require('@grpc/proto-loader');// import protoLoader module
+const path = require('path');// import path module
+
+const PROTO_PATH = path.join(__dirname,'/../protos/pollution_tracker.proto');// path to proto file
+const packageDefinition = protoLoader.loadSync(PROTO_PATH);// load proto file
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).pollution;// load package definition
+
