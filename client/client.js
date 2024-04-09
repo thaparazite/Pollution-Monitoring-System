cc85f17f9c34944b6be351676b48298b44764f49
Flaviu Vanca <skety_tm@yahoo.com.com>
2024-04-09 17:45:28 +0100

    commit

--- client/client.js
+++ client/client.js
@@ -53,7 +53,7 @@ const rl = readline.createInterface({
 });// end of readline interface
 
 
-
+var num;
 // main function
 function main() {
 
@@ -65,7 +65,7 @@ function main() {
         displayOptions();// display options
 
         rl.question('Enter the number of the service you want to display: ',(answer) => {
-
+            num = parseInt(answer);// convert answer to integer
             console.log('-------------------------------------------------------------');// print separator
             switch (answer) {
                 case '1':
@@ -93,16 +93,49 @@ function main() {
 
     }// end of askQuestion function
 
+    var counter1 = 0,counter2 = 0;// initialize counters
     // function to display service
     function displayService(client,service,title) {
 
         const call = client[service]({});// create a call to the service
 
         call.on('data',data => {
+            /*
+             * control the continue option
+             * when the user selects option 4            
+             *
+             */
+            if (num === 4) {
+                ++counter2;// increment counter2
+                if (counter2 === 3) {
+                    counter1 = 0;// reset counter1
+                    counter2 = 0;// reset counter2
+                    ++counter1;// increment counter1
+                }// end of if
+            } else {
+                ++counter1;// increment counter1
+            }// end of if else
 
+            // print data in table format 
             console.log(`\n${title}`);// print service title
             console.table(data);// print data in table format
 
+            if (counter1 === 1) {
+                console.log('-------------------------------------------------------------');// print separator
+                rl.question('Do you want to continue? (yes/no): ',(continueAnswer) => {
+                    console.log('-------------------------------------------------------------');// print separator
+                    if (continueAnswer.toLowerCase() === 'yes') {
+                        askQuestion();// ask the question again
+                    } else {
+                        console.log('-------------------------------------------------------------');// print separator
+                        console.log('\t *** End of Pollution Monitoring System ***');// print end message
+                        console.log('-------------------------------------------------------------');// print separator
+                        rl.close();// close readline interface
+                        process.exit(0);// exit the process
+                    }// end of if else
+                });
+                counter1 = 0;// reset counter1
+            }// end of if block
         });// end of call.on
 
     }// end of displayService function
