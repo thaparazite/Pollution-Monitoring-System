7a91cc2f8d495db1efe8c9b8b564a309642ef0ea
Flaviu Vanca <skety_tm@yahoo.com.com>
2024-04-09 14:10:34 +0100

    commit

--- server/server.js
+++ server/server.js
@@ -4,5 +4,18 @@ const path = require('path');// import path module
 
 const PROTO_PATH = path.join(__dirname,'/../protos/pollution_tracker.proto');// path to proto file
 const packageDefinition = protoLoader.loadSync(PROTO_PATH);// load proto file
-const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).pollution;// load package definition
+const protoDescriptor = grpc.loadPackageDefinition(packageDefinition).pollution;// load pollution package 
+
+// function that generates random pollution data
+function generateRandomData() {
+
+    return {
+        locationId: ((Math.random() * 100).toFixed(6) + " , " + (Math.random() * -200).toFixed(6)).toString(),
+        temperature: ((Math.random() * 50 + 10).toFixed(4)),
+        humidity: ((Math.random() * 100).toFixed(4)),
+        airQuality: ((Math.random() * 100).toFixed(4)),
+        noiseLevel: ((Math.random() * 100).toFixed(4)),
+    };// end of return statement
+
+}// end of generateRandomData function
 
